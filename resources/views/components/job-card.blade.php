42f77442690b73c4be0f37773e3b8dca8e67460f
Thomasplie <1039350@hr.nl>
2026-01-03 23:45:11 +0100

    Made static layout and a couple components. Messed around with tailwind set-up. added images.

---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,18 @@
+<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+    <div class="self-start text-sm">Laracasts</div>
+
+    <div class="py-8 font-bold">
+        <h3>Title</h3>
+        <p>Salary line</p>
+    </div>
+
+    <div class="flex justify-between items-center mt-auto">
+        <div>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+        </div>
+
+        <img src="{{ Vite::asset('resources/images/placeholder.png') }}" class="size-12 rounded-xl" alt="">
+    </div>
+</div>
