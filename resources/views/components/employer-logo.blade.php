eed176eedc25c3a2867e4fc601c855fe30f1d652
Thomasplie <1039350@hr.nl>
2026-01-04 01:24:29 +0100

    Finalized styling homepage, started form. Created more components.

---
+++ resources/views/components/employer-logo.blade.php
@@ -0,0 +1,3 @@
+@props(['size' => 90])
+<img src="{{ Vite::asset('resources/images/placeholder.png') }}" class="w-[{{ $size }}px] h-[{{ $size }}px] rounded-xl"
+     alt="">
--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,18 +1,18 @@
-<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+<x-panel class="flex flex-col text-center">
     <div class="self-start text-sm">Laracasts</div>
 
-    <div class="py-8 font-bold">
-        <h3>Title</h3>
-        <p>Salary line</p>
+    <div class="py-8">
+        <h3 class="group-hover:text-blue-800 transition-colors duration-300 text-xl font-bold">Title</h3>
+        <p class="text-sm mt-4">Salary line</p>
     </div>
 
     <div class="flex justify-between items-center mt-auto">
         <div>
-            <x-tag>Tag</x-tag>
-            <x-tag>Tag</x-tag>
-            <x-tag>Tag</x-tag>
+            <x-tag size="small">Backend</x-tag>
+            <x-tag size="small">Frontend</x-tag>
+            <x-tag size="small">API</x-tag>
         </div>
 
-        <img src="{{ Vite::asset('resources/images/placeholder.png') }}" class="size-12 rounded-xl" alt="">
+        <x-employer-logo/>
     </div>
-</div>
+</x-panel>
